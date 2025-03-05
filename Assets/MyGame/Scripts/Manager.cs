cbd9ef8f36e9251d652744aff8f91d37fccb3309
Davdi <david.gildinger07@htl-salzburg.ac.at>
2025-03-05 19:58:32 +0100

    final Commit thu

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -1,20 +1,15 @@
+using Palmmedia.ReportGenerator.Core.Reporting.Builders;
 using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
     private GameObject cube;
-    private GameObject cube1; //Block links oben
-    private GameObject cube2; //Block links unten
-    private GameObject cube3; //Block rechts unten
-    private GameObject cube4; //Block rechts oben
-    private GameObject cube5; //Block mitte oben
-    private GameObject cube6; //Block mitte unten
-    private GameObject cube7; //Block mitte mitte
-
+    private GameObject cube1, cube2, cube3, cube4, cube5, cube6, cube7;
     private float rotationSpeed = 100f;
     private bool hasRotated = false;
     private bool isRotating = false;
     private float targetRotation = 0f;
+    private int lastPressedNumber = -1;
 
     void Start()
     {
@@ -26,17 +21,7 @@ public class Manager : MonoBehaviour
         cube5 = GameObject.FindWithTag("Num5");
         cube6 = GameObject.FindWithTag("Num6");
         cube7 = GameObject.FindWithTag("Num7");
-
-        targetRotation = cube.transform.eulerAngles.y; // Setzt Target Roatation auf die aktuelle Y Richtung
-
-        if (cube && cube1 && cube2 && cube3 && cube4 && cube5 && cube6 && cube7 != null)
-        {
-            Debug.Log("Alle cubes gefunden.");
-        }
-        else
-        {
-            Debug.Log("Nicht alle Cubes gefunden.");
-        }
+        targetRotation = cube.transform.eulerAngles.y;
     }
 
     private void ResetColor()
@@ -50,170 +35,122 @@ public class Manager : MonoBehaviour
         cube7.GetComponent<Renderer>().material.color = Color.black;
     }
 
+    private void SetNumberColor(int number)
+    {
+        ResetColor();
+        switch (number)
+        {
+            case 1:
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube4.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 2:
+                cube2.GetComponent<Renderer>().material.color = Color.white;
+                cube4.GetComponent<Renderer>().material.color = Color.white;
+                cube5.GetComponent<Renderer>().material.color = Color.white;
+                cube6.GetComponent<Renderer>().material.color = Color.white;
+                cube7.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 3:
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube4.GetComponent<Renderer>().material.color = Color.white;
+                cube5.GetComponent<Renderer>().material.color = Color.white;
+                cube6.GetComponent<Renderer>().material.color = Color.white;
+                cube7.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 4:
+                cube1.GetComponent<Renderer>().material.color = Color.white;
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube4.GetComponent<Renderer>().material.color = Color.white;
+                cube7.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 5:
+                cube1.GetComponent<Renderer>().material.color = Color.white;
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube5.GetComponent<Renderer>().material.color = Color.white;
+                cube6.GetComponent<Renderer>().material.color = Color.white;
+                cube7.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 6:
+                cube1.GetComponent<Renderer>().material.color = Color.white;
+                cube2.GetComponent<Renderer>().material.color = Color.white;
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube5.GetComponent<Renderer>().material.color = Color.white;
+                cube6.GetComponent<Renderer>().material.color = Color.white;
+                cube7.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 7:
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube4.GetComponent<Renderer>().material.color = Color.white;
+                cube5.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 8:
+                cube1.GetComponent<Renderer>().material.color = Color.white;
+                cube2.GetComponent<Renderer>().material.color = Color.white;
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube4.GetComponent<Renderer>().material.color = Color.white;
+                cube5.GetComponent<Renderer>().material.color = Color.white;
+                cube6.GetComponent<Renderer>().material.color = Color.white;
+                cube7.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 9:
+                cube1.GetComponent<Renderer>().material.color = Color.white;
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube4.GetComponent<Renderer>().material.color = Color.white;
+                cube5.GetComponent<Renderer>().material.color = Color.white;
+                cube6.GetComponent<Renderer>().material.color = Color.white;
+                cube7.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+            case 0:
+                cube1.GetComponent<Renderer>().material.color = Color.white;
+                cube2.GetComponent<Renderer>().material.color = Color.white;
+                cube3.GetComponent<Renderer>().material.color = Color.white;
+                cube4.GetComponent<Renderer>().material.color = Color.white;
+                cube5.GetComponent<Renderer>().material.color = Color.white;
+                cube6.GetComponent<Renderer>().material.color = Color.white;
+                Debug.Log($"Zahl {lastPressedNumber} gedrückt");
+                break;
+        }
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        for (int i = 0; i <= 9; i++)
         {
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube4.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha1))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            cube2.GetComponent<Renderer>().material.color = Color.white;
-            cube4.GetComponent<Renderer>().material.color = Color.white;
-            cube5.GetComponent<Renderer>().material.color = Color.white;
-            cube6.GetComponent<Renderer>().material.color = Color.white;
-            cube7.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha2))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube4.GetComponent<Renderer>().material.color = Color.white;
-            cube5.GetComponent<Renderer>().material.color = Color.white;
-            cube6.GetComponent<Renderer>().material.color = Color.white;
-            cube7.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha3))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            cube1.GetComponent<Renderer>().material.color = Color.white;
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube4.GetComponent<Renderer>().material.color = Color.white;
-            cube7.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha4))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            cube1.GetComponent<Renderer>().material.color = Color.white;
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube5.GetComponent<Renderer>().material.color = Color.white;
-            cube6.GetComponent<Renderer>().material.color = Color.white;
-            cube7.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha5))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            cube1.GetComponent<Renderer>().material.color = Color.white;
-            cube2.GetComponent<Renderer>().material.color = Color.white;
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube5.GetComponent<Renderer>().material.color = Color.white;
-            cube6.GetComponent<Renderer>().material.color = Color.white;
-            cube7.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha6))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube4.GetComponent<Renderer>().material.color = Color.white;
-            cube5.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha7))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha8))
-        {
-            cube1.GetComponent<Renderer>().material.color = Color.white;
-            cube2.GetComponent<Renderer>().material.color = Color.white;
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube4.GetComponent<Renderer>().material.color = Color.white;
-            cube5.GetComponent<Renderer>().material.color = Color.white;
-            cube6.GetComponent<Renderer>().material.color = Color.white;
-            cube7.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha8))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha9))
-        {
-            cube1.GetComponent<Renderer>().material.color = Color.white;
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube4.GetComponent<Renderer>().material.color = Color.white;
-            cube5.GetComponent<Renderer>().material.color = Color.white;
-            cube6.GetComponent<Renderer>().material.color = Color.white;
-            cube7.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha9))
-        {
-            ResetColor();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            cube1.GetComponent<Renderer>().material.color = Color.white;
-            cube2.GetComponent<Renderer>().material.color = Color.white;
-            cube3.GetComponent<Renderer>().material.color = Color.white;
-            cube4.GetComponent<Renderer>().material.color = Color.white;
-            cube5.GetComponent<Renderer>().material.color = Color.white;
-            cube6.GetComponent<Renderer>().material.color = Color.white;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Alpha0))
-        {
-            ResetColor();
-        }
-
-        {
-            if (!hasRotated && !isRotating && (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) ||
-                Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Alpha4) ||
-                Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Alpha6) ||
-                Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Alpha8) ||
-                Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Alpha0)))
+            if (Input.GetKeyDown(KeyCode.Alpha0 + i))
             {
-                targetRotation += 180f;
-                isRotating = true;
-            }
-
-            if (isRotating)
-            {
-                float step = rotationSpeed * Time.deltaTime;
-                cube.transform.rotation = Quaternion.RotateTowards(cube.transform.rotation, Quaternion.Euler(0, targetRotation, 0), step);
-
-                if (Quaternion.Angle(cube.transform.rotation, Quaternion.Euler(0, targetRotation, 0)) < 0.1f)
+                if (lastPressedNumber != i)
                 {
-                    cube.transform.rotation = Quaternion.Euler(0, targetRotation, 0);
-                    isRotating = false;
+                    lastPressedNumber = i;
+                    SetNumberColor(i);
+                }
+                if (!hasRotated)
+                {
+                    targetRotation += 180f;
+                    isRotating = true;
                     hasRotated = true;
                 }
             }
         }
-    }   
+
+        if (isRotating)
+        {
+            float step = rotationSpeed * Time.deltaTime;
+            cube.transform.rotation = Quaternion.RotateTowards(cube.transform.rotation, Quaternion.Euler(0, targetRotation, 0), step);
+            if (Quaternion.Angle(cube.transform.rotation, Quaternion.Euler(0, targetRotation, 0)) < 0.1f)
+            {
+                cube.transform.rotation = Quaternion.Euler(0, targetRotation, 0);
+                isRotating = false;
+            }
+        }
+    }
 }
